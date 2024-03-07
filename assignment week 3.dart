87fb133338d00e4290edeec5e3cd92eb15e41cb9
Godgithubrit <161836869+Godgithubrit@users.noreply.github.com>
2024-03-07 21:08:04 +0300

    Add files via upload

---
+++ assignment week 3.dart
@@ -0,0 +1,12 @@
+
+import 'dart:io';
+
+void main(){
+  print("enter a number");
+  int number = int.parse(stdin.readLineSync()!);
+  if(number > 10) {
+    print('your number is greater than 10');
+  }else{
+    print('your number is less than 10');10
+  }
+}
